e0e148871fd4a0a3a57006c5d8cf85301166dfde
+ <+>
2022-07-12 10:50:05 -0400

    manual merge

--- my-app/src/components/Explore.js
+++ my-app/src/components/Explore.js
@@ -1,17 +1,46 @@
-import React from "react";
-import "../Explore.css"
+
+import React, { useState, useEffect } from 'react';
+import '../Explore.css'
 
 function Explore() {
-    return (
-        <div>
-            <button>By Preferences</button>
-            <button>By Location</button>
-            <h2>This is our explore!</h2>
-            
-            
-        </div>
-    )
 
-}
+    const [favData, setFavData] = useState([]);
+    const [view, setView] = useState('favorites');
+
+    useEffect(() => {
+        fetch(`http://localhost:3001/favorited_content`)
+        .then((r) => r.json())
+        .then((data) => setFavData(data))
+    }, []);
+
+    const displayFavImage = favData.map((fav) => <img key={fav.id} className="favImg" src={fav.image} alt='One of your favorited photos.' />)
+
+    const displayFavData = favData.map((fav) => {
+        return (
+            <div className='fav-image-container'>
+                <img key={fav.id} className="favImg" src={fav.image} alt='One of your favorited photos.' />
+                <div className='photoDetails'>
+                <h2>{fav.name}</h2>
+                <h3>{fav.location}</h3>
+                {fav.catgories.map((cat) => {
+                    return <h5>{cat.toUpperCase()}</h5>
+                })}
+                </div>
+            </div>
+        );
+    });
+
+    return (
+        <>
+            <div className='btnContainer'>
+                <button className='navBtn' onClick={() => setView('favorites')}>Favorites</button>
+                <button className='navBtn' onClick={() => setView('locations')}>Locations</button>
+            </div>
+            <div className='favBoxesContainer' >
+                {displayFavData}
+            </div>
+        </>
+        )
+};
 
 export default Explore; 
