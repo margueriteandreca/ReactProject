8bb0e24b59ec19e846764a9ddf94f11ff9e45d63
+ <+>
2022-07-12 10:31:50 -0400

    feed edits

--- my-app/src/components/SuggestedFollower.js
+++ my-app/src/components/SuggestedFollower.js
@@ -1,12 +1,15 @@
 import React from "react";
+import "../Feed.css";
 
 function SuggestedFollower() {
 
     return (
         <div>
             <h4>Try following:</h4>
-            <p>Hayley Richards</p>
+            <div className="follower-div"></div>
+            <div className="follower-name"><p>Hayley Richards</p>
             <p>@haleyrichards</p>
+            </div>
         </div>
     )
 
